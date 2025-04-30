bedbfd6a7e757c728129f5b6689b2d256a994028
Mario Esho <mario.esho@outlook.com>
2025-04-29 23:06:04 -0400

    add filter and sorting

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,13 +1,23 @@
 import { NgFor } from '@angular/common';
 import { Component, effect, input, viewChild } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import { ReportItem } from '@models';
 
 @Component({
   selector: 'app-report',
-  imports: [NgFor, MatTableModule, MatPaginatorModule],
+  imports: [
+    NgFor,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
+  ],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
@@ -15,13 +25,19 @@ export class ReportComponent {
   displayedColumns: (keyof ReportItem)[] = ['UPC', 'Name', 'Total Qty', 'Total Amount'];
   data = input.required({ transform: this.transformData });
   paginator = viewChild.required(MatPaginator);
+  sort = viewChild.required(MatSort);
 
   constructor() {
     effect(() => {
-      if (!this.paginator()) return;
-
       this.data().paginator = this.paginator();
+      this.data().sort = this.sort();
     });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.data().filter = filterValue.trim().toLowerCase();
+    this.data().paginator?.firstPage();
   }
 
   private transformData(value: ReportItem[]): MatTableDataSource<ReportItem> {
