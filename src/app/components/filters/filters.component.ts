5cb31ca03fa350e8f7db3cb8f6c6c399ce708082
Mario Esho <mario.esho@outlook.com>
2025-05-03 23:27:20 -0400

    add filter mobile view

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,6 +1,8 @@
-import { Component, output } from '@angular/core';
+import { Component, OnDestroy, output } from '@angular/core';
 import { FormGroup, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButtonModule } from '@angular/material/button';
@@ -20,10 +22,12 @@ import { UtilityService } from '@services';
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss'
 })
-export class FiltersComponent {
-  filters = output<Filters>();
-
+export class FiltersComponent implements OnDestroy {
+  public filters = output<Filters>();
   public filterForm: FormGroup;
+  public flexDirection = 'flex-direction-row';
+
+  private subscriptions = new Subscription();
 
   public get maxDate(): Date | null {
     const startDate = this.filterForm.get('startDate')?.value;
@@ -33,11 +37,26 @@ export class FiltersComponent {
     return this.utilityService.addDays(startDate, 99);
   }
 
-  constructor(private fb: FormBuilder, private utilityService: UtilityService) {
+  constructor(
+    private fb: FormBuilder,
+    private breakpointObserver: BreakpointObserver,
+    private utilityService: UtilityService) {
+    this.breakpointObserver.observe([Breakpoints.XSmall])
+      .subscribe(result => {
+        if (result.matches) {
+          this.flexDirection = 'flex-direction-column';
+        } else {
+          this.flexDirection = 'flex-direction-row';
+        }
+      });
     this.filterForm = this.fb.group({
       startDate: [null, Validators.required],
       endDate: [null, Validators.required]
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   onSubmit() {
